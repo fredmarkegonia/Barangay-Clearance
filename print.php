b91d48390ae91d8e0c54970ae4323c09781f9f75
fredmarkegonia <36651703+fredmarkegonia@users.noreply.github.com>
2019-03-04 19:43:50 -0800

    4th commit

---
+++ print.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<title>BG</title>
+<body>
+		<center><h1>Republic of the Philippines</h1>
+		<h4>Province of Misamis Occidental</h4>	
+		<h4>City of Oroquieta</h4>
+		<h4>BARANGAY TABOC SUR</h4></center>
+
+		
+		<center><h2>BARANGAY CLEARANCE</h2></center>
+			
+		<p>To Whom It May Concern:</p>
+				<blockquote><p>This is to certify that <input type ="text" name="name" id="name" value="" placeholder="name">,
+				legal age, <input type ="text" name="status" id="status" value="" placeholder="status">, a bonafide resident of Barangay Taboc 
+				Sur, Oroquieta City is personally known by the undersigned as peaceful
+				and law abiding citizen in the community</p></blockquote>			
+
+				<blockquote><p>This is to certifies that he/she has no pending case or criminal
+				involving moral turpitude neither he/she charge for any criminal case
+				in the community or per records in the Barangay.</p></blockquote>
+				
+				<blockquote><p>This is to certification is issued upon the request of the aboved-
+				named person in connection with his/her desire <input type ="text" name="purpose" id="purpose" value="" placeholder="purpose">.</p></blockquote>
+			<blockquote><p>Given this <input type ="text" name="day" id="purpose" value="" placeholder="day"> day of <input type ="text" name="month" id="month" value="" placeholder="month">, 20<input type ="number" name="year" id="year" value="" placeholder="year"> at 
+			Barangay Taboc Sur, Oroquieta City, Philippines.</p></blockquote>
+			
+			
+			
+			<br><br><br><center><input type ="text" name="name" id="name" value="" placeholder="name">
+			<h4>PUNONG BARANGAY</h4></br></br></br></center>
+			
+			<center><input class="btn btn-primary" type="button" onclick="window.print()" value="Print page"/>
+			<a href ="index.php"><input class="btn" type="button" id="list_btn" value="Back"/><br></a>
+
+</body>
+</html>
